5a2c32e3112b46ef9fcb06a541fc771109c1a1ba
[RealFilllykung] <filllykunggm@gmail.com>
2020-09-18 21:56:04 +0700

    Test whether Vuetify works or not which it works

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +9,11 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      auth: true,
+      title: 'Filllykung - Personal Portfolio'
+    }
   },
   {
     path: '/about',
@@ -16,13 +21,20 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About,
+    meta: {
+      title: 'About Filllykung'
+    }
   }
 ]
 
 const router = new VueRouter({
   routes,
   mode:"history"
+})
+router.beforeEach((to,from,next) => {
+  document.title = to.meta.title
+  next()
 })
 
 export default router
